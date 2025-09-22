b98ac5a8b0b93de16e585c158b16ba872211e595
Satish <Satish141m@gmail.com>
2025-09-22 19:56:37 +0530

    Update DataType.cpp

--- Data Types & Variables/DataType.cpp
+++ Data Types & Variables/DataType.cpp
@@ -1,8 +1,19 @@
- #include <iostream>
+#include <iostream>
+#include <string>
 
- int main()
- {
-    std::cout<<"Hello";
+int main() {
+    int age = 20;
+    float pi = 3.14f;
+    double price = 99.999;
+    char grade = 'A';
+    bool isAlive = true;
+    std::string name = "Blade";
 
+    std::cout << "Age: " << age << std::endl;
+    std::cout << "Pi: " << pi << std::endl;
+    std::cout << "Price: " << price << std::endl;
+    std::cout << "Grade: " << grade << std::endl;
+    std::cout << "Alive: " << isAlive << std::endl;
+    std::cout << "Name: " << name << std::endl;
     return 0;
- }
+}
