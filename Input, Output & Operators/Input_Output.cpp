740241939f3283f55641be6572d33ab18133c96d
Satish <Satish141m@gmail.com>
2025-09-24 23:59:23 +0530

    Create Input_Output.cpp

---
+++ Input, Output & Operators/Input_Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+
+    std::cout<<"Enter your name: ";
+    std::cin>>name;
+
+    std::cout<<"Name: "<<name<<std::endl;
+
+    return 0;
+}
