82de02e4ca2b24d0aeeb9ead4b08290974e7384f
Satish <Satish141m@gmail.com>
2025-09-26 23:09:50 +0530

    Create Solutions.cpp

---
+++ Data Types & Variables/Solutions.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <math.h>
+
+
+int main()
+{
+    /*
+
+    Q1.
+    -------------------------------->
+
+    std::cout << "Size of int: " << sizeof(int) << "bytes" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << "bytes" <<std::endl;
+    std::cout << "Size of double: " << sizeof(double) << "bytes" <<std::endl;
+    std::cout << "Size of char: " << sizeof(char) << "bytes" <<std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " bytes" << std::endl;
+
+    Q2.
+    -------------------------------->
+
+    int a = 2;
+    float b = 3.14f;
+    double c = 22.4;
+    char d = 'A';
+    long e = 14062002L;
+    bool val = true;
+    
+    std::cout << "int: " << a << "\n";
+    std::cout << "float: " << b << "\n";
+    std::cout << "double: " << c << "\n";
+    std::cout << "char: " << d << "\n";
+    std::cout << "long: " << e << "\n";
+    std::cout << "bool: " << val << "\n";
+
+    Q3.
+    -------------------------------->
+
+    int a = 10, b = 20, c;
+    c = a;
+    a = b;
+    b = c;
+
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+
+    Q4.
+    -------------------------------->
+
+    int a = 10, b = 20;
+    a = b + a;
+    b = a - b;
+    a = a - b;
+    
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+
+    Q5.
+    -------------------------------->
+
+    double r = 2, pi = 31.4f, area;
+
+    area = pi * (r * r);
+    std::cout << "area of circle = " << area << std::endl;
+
+    Q6.
+    -------------------------------->
+
+    float e = 4;
+    double area = 6 * (e * e);
+    std::cout << "surface area of cube = " << area << std::endl;
+
+    Q7.
+    -------------------------------->
+    
+    float e = 5;
+    double volume = e * e* e;
+
+    std::cout << "volume of cube = " << volume << std::endl;
+
+    Q8.
+    -------------------------------->
+    
+    double area, r = 2;  
+    
+    area = 4 * M_PI * (r * r);
+    std::cout << "surface area of a sphere = " << area << std::endl;
+    
+    Q8.
+    -------------------------------->
+
+    double volume, r = 6;
+
+    volume = 4.0/3 * M_PI * (r * r * r);
+    std::cout << "volume of sphere = " << volume << std::endl; 
+
+    Q9.
+    -------------------------------->
+
+    double area, h, r = 2,
+
+    area = 2 * M_PI * r * (h + 2) * M_PI * (r * r);
+    std::cout << "surface area of a cylinder = " << area << std::endl;
+
+    Q9.
+    -------------------------------->
+
+    double volume, r = 4, h = 2;
+    
+    volume = M_PI * (r * r) * h;
+    std::cout << "volume of a cylinder = " << volume << std::endl;
+
+    Q10.
+    -------------------------------->
+    
+    double volume, r = 4, h = 2;
+
+    volume = M_PI * (r * r) * h/3.0;
+    std::cout << "volume of a cone = " << volume << std::endl;
+
+    Q11.
+    -------------------------------->
+
+    double si, p = 1000 , r = 5, t = 2;
+    si = p * r * t / 100;
+    std::cout << "simple interest = " << si << std::endl; 
+
+    Q12.
+    -------------------------------->
+    double fahrenheit = 45.2, celsius = 24.60, f, c;
+
+    f = (celsius * 9.0/5) + 32; 
+    std::cout << "24.60 Celsius to Fahrenheit = " << f << std::endl; 
+
+    c = (fahrenheit - 32) * 5.0/9;
+    std::cout << "45.2 Fahrenheit to Celsius = " << c << std::endl;
+
+    */
+  return 0;
+}
