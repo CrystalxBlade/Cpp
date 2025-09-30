709a8a5ed5add5accdcffac44312cdc2d6038f69
Satish <Satish141m@gmail.com>
2025-10-01 00:10:47 +0530

    else if

---
+++ 3 - Conditional Statements/Solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    /*
+
+    */
+    int num;
+    std::cout << "Enter num: "; 
+    std::cin >> num;
+    if (num % 2 == 0)
+    {
+        std::cout << "Even";
+    }
+    else
+    {
+        std::cout << "Odd";
+    }
+
+    return 0;
+    
+}
