669fce6d610c7caca95c1a40e3d81ee5ce51d4a4
Satish <Satish141m@gmail.com>
2025-10-07 18:54:12 +0530

    Create Solution.cpp

---
+++ 4 - Loops/Solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    /*
+
+    Q1.
+    -------------------------------->
+    
+    int num = 1234, rev = 0, rem;
+
+    while (num > 0)
+    {
+        rem = num%10;
+        rev = rev*10 + rem;
+        num = num/10;
+    }
+
+    std::cout <<"Reverse num: " << rev << std::endl;
+
+    */
+
+    return 0;
+}
