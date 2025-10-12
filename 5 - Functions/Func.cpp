8de3bb048b7e151ad11002c27197cd07b21e1c95
Satish <Satish141m@gmail.com>
2025-10-12 18:37:00 +0530

    Create Func.cpp

---
+++ 5 - Functions/Func.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+// Non Parameterized, without Return type
+void Func()
+{
+    std::cout << "1. Non Parameterized without Return type\n";
+}
+
+// Non Parameterized, with Return type (returns a string)
+std::string FuncR()
+{
+    return "2. Non Parameterized with Return type";
+}
+
+// Parameterized, without Return type (void)
+void FuncP(int a, int b)
+{
+    std::cout << "3. Parameterized without Return type: Sum is " << a + b << "\n";
+}
+
+// Parameterized, with Return type (returns int)
+int FuncPR(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    Func();
+    
+    std::string resultR = FuncR();
+    std::cout << resultR << "\n";
+    
+    FuncP(2, 4);
+    
+    std::cout << "4. Parameterized with Return type: Returned value is " << FuncPR(4, 6) << "\n";
+    
+    return 0;
+}
