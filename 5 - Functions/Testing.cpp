16103e178069ca353d2e76ad5413d08a9841097c
Satish <Satish141m@gmail.com>
2025-10-13 20:19:32 +0530

    Update Testing.cpp

--- 5 - Functions/Testing.cpp
+++ 5 - Functions/Testing.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+int Sum(int num)
+{
+    int rem, sum = 0;
+    while (num > 0)
+    {
+        rem = num % 10;
+        sum = sum + rem;
+        num = num / 10;
+    }
+    return sum;
+}
+
 int main()
 {
-    
+    std::cout << "Sum of all digits is: " << Sum(145) << std::endl;
     return 0;
 }
