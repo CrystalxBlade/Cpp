67712f6fc15b31707e375ddb400cb9537de1883d
Satish <Satish141m@gmail.com>
2025-10-17 17:56:28 +0530

    Update Patterns.cpp

--- 4 - Loops/Patterns.cpp
+++ 4 - Loops/Patterns.cpp
@@ -49,13 +49,22 @@ int main()
         std::cout << "\n";
     }
 
-    */
-
     for(int i = 1; i <= 4; i++)
     {
         for(int j = 1; j <= i; j++)
         {
             std::cout << j  << " ";
+        }
+        std::cout << "\n";
+    }
+
+    */
+
+    for(int i = 1; i <= 4; i++)
+    {
+        for(int j = i; j > 0; j--)
+        {
+            std::cout << j << " "; 
         }
         std::cout << "\n";
     }
