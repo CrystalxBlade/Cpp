39673aeb53f664c225e70f35a203e8b6b5471e7d
Satish <Satish141m@gmail.com>
2025-10-18 13:30:41 +0530

    Update Patterns.cpp

--- 4 - Loops/Patterns.cpp
+++ 4 - Loops/Patterns.cpp
@@ -85,7 +85,22 @@ int main()
             std::cout << num << " ";
         }
         std::cout << "\n";
+
+        Inverted Triangle
     }
 
     */
+
+    for(int i = 1; i <=4; i++)
+    {
+        for(int k = 1; k < i; k++)
+        {
+            std::cout << " ";
+        }
+        for(int j = 4; j >= i; j--)
+        {
+            std::cout << i;
+        }
+        std::cout << "\n";
+    }
 }
