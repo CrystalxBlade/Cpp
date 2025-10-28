6cacd12de7caeaf94ab930bb99fd79faba5f6b27
CrystalxBlade <satish141m@gmail.com>
2025-10-28 20:37:22 +0530

    testing

--- 6 - Arrays/Testing.cpp
+++ 6 - Arrays/Testing.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-int main()
-{
     /*
 
     int size = 5;
@@ -112,8 +110,14 @@ int main()
 
     */
 
+void sort(int arr[], int size);
+
+int main()
+{
     int arr[] = {10, 2, 9, 3, 7, 4, 6, 5, 1, 8};
     int size = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, size);
 
     for(int element : arr)
     {
@@ -125,5 +129,17 @@ int main()
 
 void sort(int arr[], int size)
 {
-        
+    int temp;
+    for(int i = 0; i < size - 1; i++)
+    {
+        for(int j = 0; j < size - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
