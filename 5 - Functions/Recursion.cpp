cf1d25d0ad6ab792e2107ca560f373a08a068ede
CrystalxBlade <satish141m@gmail.com>
2025-10-29 19:55:31 +0530

    recursion

--- 5 - Functions/Recursion.cpp
+++ 5 - Functions/Recursion.cpp
@@ -4,13 +4,17 @@ void countdown(int n)
 {
     if (n == 0)
     {
-        
+        std::cout << "Boom" << std::endl;
+        return;
     }
     
+    std::cout << n << std::endl;
+
+    countdown(n - 1);
 }
 
 int main()
 {
-
+    countdown(5);
     return 0;
 }
