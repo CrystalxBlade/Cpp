3c953fc459e2dde88ba64017324423318d579c94
CrystalxBlade <satish141m@gmail.com>
2025-10-31 13:02:10 +0530

    s

--- 6 - Arrays/Solution.cpp
+++ 6 - Arrays/Solution.cpp
@@ -220,34 +220,28 @@
 
     */
 
-    void bubbleSort(int arr[], int n)
+
+int main()
+{
+
+    int arr[] = {5, 1, 4, 3, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n - 1; i++)
     {
-        for(int i = 0; i < n-1; i++)
+        for(int j = 0; j < n - 1; j++)
         {
-            for(int j = 0; j<n-i-1; j++)
+            if(arr[j] > arr[j + 1])
             {
-                if(arr[j] > arr[j + 1])
-                {
-                    std::swap(arr[j], arr[j+1]);
-                }
+                std::swap(arr[j], arr[j + 1]);
             }
         }
     }
 
-    void printArray(int arr[], int n)
+    for(int i : arr)
     {
-        for(int i = 0; i < n; i++)
-        {
-            std::cout << arr[i] << " ";
-        }
+        std::cout << i << " ";
     }
 
-    int main()
-    {
-        int n = 5;
-        int arr[] = {4, 1, 5, 2, 3};
-
-        bubbleSort(arr, n);
-        printArray(arr, n);
-        return 0;
-    }
+    return 0;
+}    
