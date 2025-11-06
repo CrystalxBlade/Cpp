4a5d25f7f16438b4c1e1443d3a2867d494ef02a5
CrystalxBlade <satish141m@gmail.com>
2025-11-06 17:55:10 +0530

    all

--- 6 - Arrays/Testing.cpp
+++ 6 - Arrays/Testing.cpp
@@ -179,20 +179,21 @@ int main()
 
 int main()
 {
-    int arr[] = {5, 1, 4, 2, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {5, 2, 4, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     for(int i = 0; i < n - 1; i++)
     {
+        int sm = i;
+
         for(int j = i + 1; j < n; j++)
         {
-            if(arr[j] < arr[i])
+            if(arr[j] < arr[sm])
             {
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+                sm = j;
             }
         }
+        std::swap(arr[i], arr[sm]);
     }
 
     for(int i : arr)
