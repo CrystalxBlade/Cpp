3dcf37a92616f4c4b587b4cd76865503c9a3c892
CrystalxBlade <satish141m@gmail.com>
2025-11-09 12:37:48 +0530

    ex

--- Exp.cpp
+++ Exp.cpp
@@ -1,6 +1,13 @@
-#include <iostream>>
+#include <iostream>
 
 int main()
 {
-    
+    int sum = 0;
+
+    for(int i = 1; i <= 10; i++)
+    {
+        sum += i;
+    }
+    std::cout << sum;
+    return 0;
 }
