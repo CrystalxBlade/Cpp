04de5ab743d82a7ff71bca57f2ec1eca846cbb9d
CrystalxBlade <satish141m@gmail.com>
2025-11-10 18:51:23 +0530

    e

--- Exp.cpp
+++ Exp.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
+void CountDown(int n)
+{
+    if(n == 0)
+    {
+        std::cout << " Booooooom ";
+        return;
+    }
+
+    std::cout << "tick tick " << n <<std::endl;
+    CountDown(n-1);
+}
+
 int main()
 {
-    int arr[] = {5, 20, 12, 3, 47};
 
-    for(int i : arr)
-    {
-        std::cout << i << " ";
-    }
+    int n;
+    std::cout << "Enter a value to start countDown: ";
+    std::cin >> n;
+
+    CountDown(n);
 
     return 0;
 }
+
