b7e4fe54b4ea560c620f58f3404158db4d5df6f9
CrystalxBlade <satish141m@gmail.com>
2025-11-10 19:23:13 +0530

    recurrsive

--- 5 - Functions/Recursion.cpp
+++ 5 - Functions/Recursion.cpp
@@ -58,3 +58,18 @@ int main()
 }
 
 */
+
+int fact(int n)
+{
+    if(n == 0 || n == 1) return 1;
+    return n * fact(n - 1);
+}
+
+int main()
+{
+    int n = 5;
+
+    std::cout << fact(n);
+
+    return 0;
+}
