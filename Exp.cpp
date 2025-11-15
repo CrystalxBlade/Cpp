6d146fbb8f39f45c597fb2d175d733151f4a77f4
CrystalxBlade <satish141m@gmail.com>
2025-11-15 20:22:10 +0530

    modified

--- Exp.cpp
+++ Exp.cpp
@@ -3,34 +3,7 @@
 
 int main()
 {
-    int arr[] = {6, 12, 29, 77, 56, 6, 29, 56, 9, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    bool visited[n] = {false};
-
-    std::cout << "Frequency of each element \n";
-
-    for(int i = 0; i < n; i++)
-    {
-        if (visited[i])
-        {
-            continue;
-        }
-        
-        int count = 1;
-
-        for(int j = i + 1; j < n; j++)
-        {
-            if(arr[j] == arr[i])
-            {
-                count++;
-                visited[j] = true;
-            }
-        }
-        
-        std::cout << arr[i] << " - " << count << " times \n";
-    }
-
+    
     return 0;
 }
 
