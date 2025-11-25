bbf4a26bf28ddc8ef64877adf74be36cb640cf4a
CrystalxBlade <satish141m@gmail.com>
2025-11-25 20:45:07 +0530

    matrix

---
+++ 6 - Arrays/2D_Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+    int matrix [4][3];
+
+    matrix [0][0] = 1;
+    matrix [0][1] = 2;
+    matrix [0][2] = 3;
+
+    matrix [1][0] = 4;
+    matrix [1][1] = 5;
+    matrix [1][2] = 6;
+
+    matrix [2][0] = 7;
+    matrix [2][1] = 8;
+    matrix [2][2] = 9;
+
+    matrix [3][0] = 10;
+    matrix [3][1] = 11;
+    matrix [3][2] = 12;
+
+    int row = 4, col = 3;
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            std::cout << matrix [i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
