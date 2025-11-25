8dd608e0450e50529f419934ef562449b275df1c
CrystalxBlade <satish141m@gmail.com>
2025-11-25 20:05:47 +0530

    don

--- 6 - Arrays/2D_Solution.cpp
+++ 6 - Arrays/2D_Solution.cpp
@@ -3,6 +3,16 @@
     Q31.
     -------------------------------->
 
-    
+
 */
 
+#include <iostream>
+
+int main()
+{
+    int matrix [4] [3] = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+
+    std::cout << matrix [3][1] << std::endl;
+
+    return 0;
+}
