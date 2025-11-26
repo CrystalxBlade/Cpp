f6a6f62ca9bae697987243beb7676dcdb7844bec
CrystalxBlade <satish141m@gmail.com>
2025-11-26 18:46:54 +0530

    sixtynine

--- 6 - Arrays/2D_Array.cpp
+++ 6 - Arrays/2D_Array.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+/*
+
 int main()
 {
     int matrix [4][3];
@@ -31,4 +33,15 @@ int main()
         std::cout << "\n";
     }
     return 0;
+}
+
+*/
+
+int main()
+{
+    int matrix [4][3] = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+
+    int pos = matrix[2][1] = 69;
+
+    std::cout << pos;
 }
