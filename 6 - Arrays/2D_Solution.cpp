b4e834bf2487c3d1d8bdd4d7672587b6248eeefb
CrystalxBlade <satish141m@gmail.com>
2025-11-27 20:23:26 +0530

    row

--- 6 - Arrays/2D_Solution.cpp
+++ 6 - Arrays/2D_Solution.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+#include <climits>
 /*
-    Q1.
+    Q31.
     -------------------------------->
 
 int main()
@@ -23,8 +23,35 @@ int main()
     return 0;
 }
 
-    Q2.
+    Q32.
     -------------------------------->
+
+
+int main()
+{
+    int matrix [3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    int m = sizeof(matrix) / sizeof(matrix[0]);
+    int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    int sum = 0;
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            sum += matrix[i][j];
+        }
+    }
+
+    std::cout << "Sum of matrix: " << sum;
+
+    return 0;
+}
+
+    Q33.
+    -------------------------------->
+
 */
 
 int main()
@@ -34,5 +61,30 @@ int main()
     int m = sizeof(matrix) / sizeof(matrix[0]);
     int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
 
+    int rowSum = 0;
+    int colSum = 0;
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            rowSum += matrix[i][j];
+        }
+
+        std::cout << "rowSum of : " << i << " - " << rowSum << " \n";
+    }
+
+    std::cout << "------------------\n";
     
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            colSum += matrix[i][j];
+
+            std::cout << "colSum of : " << j <<  " - " << colSum << " \n";
+        }
+    }
+
+    return 0;
 }
