c30c3828aded02e2d1c3fe0ec450de709a0068c7
CrystalxBlade <satish141m@gmail.com>
2025-12-01 21:51:22 +0530

    new

--- 6 - Arrays/Testing.cpp
+++ 6 - Arrays/Testing.cpp
@@ -298,4 +298,39 @@ int main()
     printMatrix(matrix, m, n, key);
 }
 
+
+
+int main()
+{
+    int matrix [3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    int m = sizeof(matrix) / sizeof(matrix[0]);
+    int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+    
+            std::cout << matrix [i][j] <<" ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout <<"\n ";
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                std::cout << matrix [i][j] <<" ";
+            }   
+        }
+    }
+
+    return 0;
+}
+
 */
