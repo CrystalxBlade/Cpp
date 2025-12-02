e5e66522eda53e31a9fd124355dd926a8f679746
CrystalxBlade <satish141m@gmail.com>
2025-12-02 18:10:54 +0530

    l

--- 6 - Arrays/2D_Solution.cpp
+++ 6 - Arrays/2D_Solution.cpp
@@ -94,6 +94,19 @@ int main()
 
 int main()
 {
-    
+    int matrix [3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    int m = sizeof(matrix) / sizeof(matrix[0]);
+    int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cout << matrix [i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
     return 0;
 }
