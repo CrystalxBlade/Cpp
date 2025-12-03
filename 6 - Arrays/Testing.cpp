ba8eec78b98a5469a51c46ac50ae1a02a2f47e7b
CrystalxBlade <satish141m@gmail.com>
2025-12-03 22:09:13 +0530

    matrix

--- 6 - Arrays/Testing.cpp
+++ 6 - Arrays/Testing.cpp
@@ -366,4 +366,31 @@ int main()
 
 */
 
+int main()
+{
+    int matrix[2][3] = {{1,2,3}, {4,5,6}};
 
+    int m = sizeof(matrix) / sizeof(matrix[0]);
+    int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cout << matrix[i][j] <<" ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "---------------------- > \n";
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cout << matrix[j][i] <<" ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
