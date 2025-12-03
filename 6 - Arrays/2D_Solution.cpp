b9843795b2461effc717ec4195e618a2b942b651
CrystalxBlade <satish141m@gmail.com>
2025-12-03 20:36:01 +0530

    secondary

--- 6 - Arrays/2D_Solution.cpp
+++ 6 - Arrays/2D_Solution.cpp
@@ -107,4 +107,25 @@ int main()
     return 0;
 }
 
+    // Secondary diagonal
 */
+int main()
+{
+    int matrix[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    int m = sizeof(matrix) / sizeof(matrix[0]);
+    int n = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if (i + j == n - 1)
+            {
+                std::cout << matrix[i][j] << " ";
+            }
+        }
+    }
+
+    return 0;
+}
