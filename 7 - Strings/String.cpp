5a7de584e7c03a7501de3ccc91c5e73b7bdfa12c
CrystalxBlade <satish141m@gmail.com>
2025-12-09 21:39:11 +0530

    character

--- 7 - Strings/String.cpp
+++ 7 - Strings/String.cpp
@@ -4,15 +4,31 @@ int main()
 {
     /*
 
+    char ch [] = {'B', 'L', 'A', 'D', 'E' '\0'}; //the sequence of characters are called string
+    std::cout << ch;
+
+    char ch [] = {'B', 'L', 'A', 'D', 'E'}; // error There is NO null terminator \0. 
+    std::cout << ch;
+
+
     std::string name = "Blade";
     std::cout << name;
-    
 
-    std::string name;
+    std::string name = "Blade"; // dynamic => runtime resize
     name = "Crystal";
     std::cout << name;
-    
+
+    char ch [] = {'R', 'O', 'C', 'K'}; // error no dynamic => runtime resize   
+    ch = {'L', 'O', 'L'};
+
+    std::string name;
+    std::cout << "Enter your Name: ";
+    std::getline(std::cin, name);
+    std::cout << "Sup My Nigga: " << name;
+
     */
+
+    
 
     return 0;
 }
