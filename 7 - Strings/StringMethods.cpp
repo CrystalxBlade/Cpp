3abf972d7e430b29a3af35cc95cae28fa754170d
CrystalxBlade <satish141m@gmail.com>
2025-12-10 19:40:54 +0530

    ne

--- 7 - Strings/StringMethods.cpp
+++ 7 - Strings/StringMethods.cpp
@@ -9,10 +9,28 @@ int main()
     std::cout << "Size: " << s.size() << std::endl;
     std::cout << "Length: " << s.length() << std::endl;
 
-     // accessing characters
+    std::cout << "\n";
+
+    // accessing characters
 
     std::cout << "Front: " << s.front() << std::endl;
     std::cout << "Back: " << s.back() << std::endl;
 
+    std::cout << "\n";
+
+    // indexing / at
+
+    std::cout << "at(4): " << s.at(5) << std::endl;
+    std::cout << "s[4]: " << s[4] << std::endl;
+
+    std::cout << "\n";
+    
+    // push_back, pop_back
+
+    s.push_back('!');
+    std::cout << "After push_back: " << s << std::endl;
+
+    s.pop_back();
+    std::cout << "After pop_back: " << s << std::endl;
     return 0;
 }
