91de6805489cc4dc896c7f95039088906e25bf81
CrystalxBlade <satish141m@gmail.com>
2025-12-14 15:18:46 +0530

    new

--- 7 - Strings/Solution.cpp
+++ 7 - Strings/Solution.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
+    /*
+
+    Q1.
+    -------------------------------->    
     
+    std::string name;
+    std::cout << "Enter name ";
+    std::getline(std::cin, name);
+    std::cout << "Sup "<< name << std::endl;
+
+    Q2.
+    -------------------------------->  
+    */
 
     return 0;
 }
