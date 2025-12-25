ab61346b03c4f76cb38fb52387d324bcba1196a7
CrystalxBlade <satish141m@gmail.com>
2025-12-25 20:05:01 +0530

    new

--- 8 - Pointers/Pointers.cpp
+++ 8 - Pointers/Pointers.cpp
@@ -9,6 +9,7 @@ int main()
     std::cout << "The address of a : " <<&a << "\n";
     std::cout << "The value of b : " << b;
 
-    
+    // * ----> (value at) Dereference Operator
+    std::cout << "The address of a is " << *b << "\n";
     return 0;
 }
