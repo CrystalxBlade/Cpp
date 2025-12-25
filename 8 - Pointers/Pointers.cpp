91c91ed7fb6271ab7a0f060fb1d5365b804a728f
CrystalxBlade <satish141m@gmail.com>
2025-12-25 20:02:10 +0530

    new

--- 8 - Pointers/Pointers.cpp
+++ 8 - Pointers/Pointers.cpp
@@ -5,7 +5,10 @@ int main()
     int a = 10;
     int* b = &a;
     
-    std::cout << "The address of a : " << &a << "\n";
-    std::cout << &a;
+    // & ----> (Address of) Operator
+    std::cout << "The address of a : " <<&a << "\n";
+    std::cout << "The value of b : " << b;
+
+    
     return 0;
 }
