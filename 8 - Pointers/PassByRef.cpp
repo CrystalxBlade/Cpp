48012d0fc48f09b496d30a511ee8a555bfd42d3d
CrystalxBlade <satish141m@gmail.com>
2025-12-26 20:24:07 +0530

    new

--- 8 - Pointers/PassByRef.cpp
+++ 8 - Pointers/PassByRef.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+
+void changA(int* ptr)
+{
+    *ptr = 20;
+}
 int main()
 {
+    int a = 10;
+    changA(&a);
+
+    std::cout << "inside main fxn: " << a << std::endl; 
     return 0;
 }
