1a68a2dd0208a8a71452bc7b99598009f6652123
CrystalxBlade <satish141m@gmail.com>
2026-01-03 20:43:54 +0530

    new

--- 9 - Structure/Struct.cpp
+++ 9 - Structure/Struct.cpp
@@ -4,9 +4,15 @@ struct student
 {
     std::string name;
     double gpa;
+    bool enrolled;
 };
 
 int main()
 {
+    student stud1;
+    stud1.name = "Spongebob";
+    stud1.gpa = 6.2;
+    
+
     return 0;
 }
