761cd89e966eb04c9e476fe0a8b2e496bfea1aaf
CrystalxBlade <satish141m@gmail.com>
2026-01-04 19:35:58 +0530

    new

--- 9 - Structure/Struct.cpp
+++ 9 - Structure/Struct.cpp
@@ -14,6 +14,14 @@ int main()
     stud1.gpa = 6.2;
     stud1.enrolled = true;
 
+    student stud2;
+    stud2.name = "Patrick";
+    stud2.gpa = 5.7;
+    stud2.enrolled = false;
+
+    std::cout << stud1.name << "\n";
+    std::cout << stud1.gpa << "\n";
+    std::cout << stud1.enrolled << "\n";
 
     std::cout << stud1.name << "\n";
     std::cout << stud1.gpa << "\n";
