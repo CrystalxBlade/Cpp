059f5acdaec0cc758e307d0ed824598c2f223dfd
CrystalxBlade <satish141m@gmail.com>
2026-01-04 19:57:46 +0530

    new

--- 9 - Structure/TypeDef.cpp
+++ 9 - Structure/TypeDef.cpp
@@ -5,10 +5,27 @@ typedef struct employee
     int id;;
     std::string name;
     double salary;
-};
+} ep;
 
 int main()
 {
+    ep emp1;
+    emp1.id = 4;
+    emp1.name = "Spongebob";
+    emp1.salary = 60.5;
+
+    ep emp2;
+    emp2.id = 5;
+    emp2.name = "Patrick";
+    emp2.salary = 4.57;
+
+    std::cout << emp1.id << "\n";
+    std::cout << emp1.name << "\n";
+    std::cout << emp1.salary << "\n";
+
+    std::cout << emp2.id << "\n";
+    std::cout << emp2.name << "\n";
+    std::cout << emp2.salary;
     
     return 0;
 }
