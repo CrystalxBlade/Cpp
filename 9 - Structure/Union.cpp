fa9e7f7ea556a55ef6c032f9657e1231d250221a
CrystalxBlade <satish141m@gmail.com>
2026-01-07 19:55:17 +0530

    new

--- 9 - Structure/Union.cpp
+++ 9 - Structure/Union.cpp
@@ -1,1 +1,19 @@
-#include
+#include <iostream>
+
+union food
+{
+    int rice;
+    char corn;
+    float potato;
+};
+
+
+int main()
+{
+    food f;
+    f.rice = 10;
+    
+    std::cout << f.rice;
+    
+    return 0;
+}
