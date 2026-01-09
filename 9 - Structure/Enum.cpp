c1770eb3000579d7371a56f0edb3f88a170b0a83
CrystalxBlade <satish141m@gmail.com>
2026-01-09 20:00:37 +0530

    new

--- 9 - Structure/Enum.cpp
+++ 9 - Structure/Enum.cpp
@@ -2,11 +2,25 @@
 
 enum Day
 {
-    Monday
-}
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
 
 int main()
 {
+    Day today = Friday;
+
+    Switch(today)
+    {
+        case Monday:
+        std::cout << "Start of the weeknd";
+        break;
+    }
 
     return 0;
     
