de057790f72f6b074e4472845251ecd1d7234a06
CrystalxBlade <satish141m@gmail.com>
2026-01-19 20:33:22 +0530

    new

--- OOPS/Class & Objects/ClassObjects.cpp
+++ OOPS/Class & Objects/ClassObjects.cpp
@@ -9,7 +9,7 @@ class Human
 
     void Play()
     {
-        std::cout << "Playing Assassin's creed" << '\n';
+        std::cout << "Playing Assassin's creed" << ;
     }
 
     void Sleep()
@@ -21,5 +21,15 @@ class Human
 
 int main()
 {
+    Human h1;
+    h1.name = "Blade";
+    h1.occupation = "Developer";
+    h1.age = 23;
+
+    std::cout << h1.name << '\n';
+    std::cout << h1.occupation << '\n';
+    std::cout << h1.age << '\n';
+
+    
     return 0;
 }
