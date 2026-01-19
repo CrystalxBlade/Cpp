187ab6fbc6a48db6120260c1b9df8f01c6e1dfdd
CrystalxBlade <satish141m@gmail.com>
2026-01-19 20:31:18 +0530

    newf

--- OOPS/Class & Objects/ClassObjects.cpp
+++ OOPS/Class & Objects/ClassObjects.cpp
@@ -7,7 +7,15 @@ class Human
     std::string occupation;
     int age;
 
-    
+    void Play()
+    {
+        std::cout << "Playing Assassin's creed" << '\n';
+    }
+
+    void Sleep()
+    {
+        std::cout << "This guy is sleeping";
+    }
 
 };
 
