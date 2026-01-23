1cd4e017e76f3893eae7a76cc4dfef5b8a764c73
CrystalxBlade <satish141m@gmail.com>
2026-01-23 13:51:40 +0530

    new

--- OOPS/Constructor & Destructor/Constructor.cpp
+++ OOPS/Constructor & Destructor/Constructor.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+class Crystal
+{
+    public:
+    int mana;
+
+    Crystal()
+    {
+        mana = 20;
+        std::cout << "Default constructor called \n";
+    }
+}
+
 int main()
 {
+    Crystal c;
+    std::
     return 0;
 }
