d9dda5ba3deb2abefdd66b3b0bf4d2e7c39b4c26
CrystalxBlade <satish141m@gmail.com>
2026-02-03 22:14:08 +0530

    new

--- OOPS/Static & Non - Static/Static.cpp
+++ OOPS/Static & Non - Static/Static.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-class 
+class Student
+{
+    // non-static (each student has their own roll number)
+    int roll;
+    // static (shared by all students)
+    static int totaSudents;
+
+    Student(int r)
+    {
+        roll = r;
+        totaSudents++; 
+    }
+
+};
+
+int Student::totaSudents = 0; 
 
 int main()
 {
