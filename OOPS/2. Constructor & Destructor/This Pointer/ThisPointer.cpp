d431f853f8d3835aaf942ba8f414059d219651e0
CrystalxBlade <satish141m@gmail.com>
2026-02-07 21:05:21 +0530

    this

--- OOPS/2. Constructor & Destructor/This Pointer/ThisPointer.cpp
+++ OOPS/2. Constructor & Destructor/This Pointer/ThisPointer.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+class Crystal
+{
+    private:
+    std::string name;
+    int age;
+
+    public:
+    Crystal(std::string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    void Display()
+    {
+        std::cout << "Name : " << name << '\n';
+        std::cout << "Age : " << age << '\n';
+    }
+};
+
 int main()
 {
+    Crystal c("Blade", 23);
+    c.Display();
+
     return 0;
 }
