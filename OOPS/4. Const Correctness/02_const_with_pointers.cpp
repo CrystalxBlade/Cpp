fcffdaeee7eb014ac550a25fcf6cc21a73fc8d4a
CrystalxBlade <satish141m@gmail.com>
2026-02-14 21:56:14 +0530

    comit

--- OOPS/4. Const Correctness/02_const_with_pointers.cpp
+++ OOPS/4. Const Correctness/02_const_with_pointers.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-// "const int * " is a pointer to a const int.
-// "int* const " 
+// "const int* " is a pointer to a const int.
+// "int* const " is a constant pointer to a int.
+//  const int* const. 
 
 int main()
 {
