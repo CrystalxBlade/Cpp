4638562300c86fcf5286b39e83fed225f0aca41c
CrystalxBlade <satish141m@gmail.com>
2026-02-16 19:22:03 +0530

    con

--- OOPS/4. Const Correctness/03_const_references.cpp
+++ OOPS/4. Const Correctness/03_const_references.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    int x = 5;
+
+    const int& ref = x;
+
+    std::cout << ref << '\n';       // ✔️ allowed (read)
+
+    // ref = 20;                    // ❌ NOT allowed (const)
+
+    x = 30;                         // ✔️ allowed (original can change)
+
+    std::cout << ref << '\n';
+
     return 0;
 }
