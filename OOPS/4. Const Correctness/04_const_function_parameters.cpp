29e81d52b41902babb7504e676d741792ea44cb1
CrystalxBlade <satish141m@gmail.com>
2026-02-16 19:44:12 +0530

    ret

---
+++ OOPS/4. Const Correctness/04_const_function_parameters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void printNumber(const int& x)
+{
+    // x = 10;   // ❌ NOT allowed
+    std::cout << x << '\n';
+}
+
+int main()
+{
+    int a = 5;
+    printNumber(a);
+
+
+    return 0;
+}
