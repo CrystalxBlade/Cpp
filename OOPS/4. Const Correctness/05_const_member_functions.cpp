02aeb5b148258bfb143d11c2650a3086dc5b97dc
CrystalxBlade <satish141m@gmail.com>
2026-02-17 13:21:27 +0530

    object

--- OOPS/4. Const Correctness/05_const_member_functions.cpp
+++ OOPS/4. Const Correctness/05_const_member_functions.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+class Player
+{
+    int hp;
+
+    public:
+    Player(int h) : hp(h) {}
+
+    int getHP() const
+    {
+        return hp;
+    }
+
+    void takeDamage(int dmg)
+    {
+        hp -= dmg;
+    }
+};
+
 int main()
 {
+     const Player p(100);
+
+     p.getHP();        // ✔️ allowed
+//   p.takeDamage(10); // ❌ NOT allowed
+
     return 0;
 }
