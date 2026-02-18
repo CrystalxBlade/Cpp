478918b230e0e4ba710913b3fb641fa536968eb3
CrystalxBlade <satish141m@gmail.com>
2026-02-18 18:36:03 +0530

    name

--- OOPS/4. Const Correctness/06_const_objects.cpp
+++ OOPS/4. Const Correctness/06_const_objects.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+class Player
+{
+    public:
+    std::string name;
+
+    Player(std::string name)
+    {
+        this->name = name;
+    }
+};
+
 int main()
 {
+    Player p("Blade"); 
+    
+    std::cout << "The name is : " << p.name << '\n';
+    
+
     return 0;
 }
