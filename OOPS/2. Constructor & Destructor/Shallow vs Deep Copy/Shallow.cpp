efadc3211e7f09423d2d23af5ee097f7891b3a59
CrystalxBlade <satish141m@gmail.com>
2026-02-20 17:36:55 +0530

    shallow

--- OOPS/2. Constructor & Destructor/Shallow vs Deep Copy/Shallow.cpp
+++ OOPS/2. Constructor & Destructor/Shallow vs Deep Copy/Shallow.cpp
@@ -2,16 +2,29 @@
 
 class Car
 {
+    public:
+
     int speed;
     int model;
 
-    public:
+    
     Car(){}
 
+    Car(const Car& c)
+    {
+        speed = c.speed;
+        model = c.model; 
+    }
 
 };
 
 int main()
 {
+    Car BMW;
+
+    BMW.speed = 200;
+    BMW.model = 2002;
+    
+    
     return 0;
 }
