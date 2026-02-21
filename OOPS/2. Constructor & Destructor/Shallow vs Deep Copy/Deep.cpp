efa427d268a77e46f637d9eaf7800ff19c658eb6
CrystalxBlade <satish141m@gmail.com>
2026-02-21 15:11:15 +0530

    deep

--- OOPS/2. Constructor & Destructor/Shallow vs Deep Copy/Deep.cpp
+++ OOPS/2. Constructor & Destructor/Shallow vs Deep Copy/Deep.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+class Blade
+{
+public:
+      
+      int* hp;
+
+      Blade(int value)
+      {
+        hp = new int(value);
+      }
+
+          // ✅ Deep copy constructor
+
+       Blade(const Blade& obj)
+       {
+        hp = new int(*obj.hp);
+       }   
+
+       ~Blade()
+       {
+        delete hp;
+        std::cout << "Destructor Called \n";
+       }
+}
+
 int main()
 {
     return 0;
