c83a60328d1f0623cb91204f4aaeb9b1bd0b048e
CrystalxBlade <satish141m@gmail.com>
2026-02-23 13:28:13 +0530

    memory

--- OOPS/5. Dynamic Memory/2_new_delete/new_delete.cpp
+++ OOPS/5. Dynamic Memory/2_new_delete/new_delete.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    // allocate memory using new
+    int* p = new int(10);
+
+    // use the memory
+    std::cout << *p << '\n';
+
+    // free memory using delete
+    delete p;
+
+    // explain in comments:
+    // forgetting delete causes memory leak
+
     return 0;
 }
