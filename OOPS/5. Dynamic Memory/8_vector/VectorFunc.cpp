ba17a25bc424566834ca8fc16f8dfc1bc26eea96
CrystalxBlade <satish141m@gmail.com>
2026-02-26 22:34:14 +0530

    front

--- OOPS/5. Dynamic Memory/8_vector/VectorFunc.cpp
+++ OOPS/5. Dynamic Memory/8_vector/VectorFunc.cpp
@@ -3,6 +3,36 @@
 
 int main()
 {
+          
+    /*                // size
+
+    std::vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+    vec.size();  
     
+                      // push back
+    std::vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+    vec.push_back('z');
+
+    for(char c : vec)
+    {
+        std::cout << c << " ";
+    }
+
+                      // pop back
+    std::vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+    vec.pop_back();
+
+    for(char c : vec)
+    {
+        std::cout << c << " ";
+    }
+        
+                     // front & back
+    std::vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+
+    std::cout << "front : " << vec.front() << '\n';
+    std::cout << "back : " << vec.back();
+
+    */
     return 0;
 }
