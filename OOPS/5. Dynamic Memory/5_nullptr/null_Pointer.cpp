181783e6ad4640e85a6db19db38a59d02906e673
CrystalxBlade <satish141m@gmail.com>
2026-02-26 20:00:48 +0530

    new

--- OOPS/5. Dynamic Memory/5_nullptr/null_Pointer.cpp
+++ OOPS/5. Dynamic Memory/5_nullptr/null_Pointer.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
+// Safety checks
+// protect from causing crashes
+
 int main()
 {
     int* p = new int(10);
     delete p;
     p = nullptr; // Pointer pointing to freed memory
 }
-
