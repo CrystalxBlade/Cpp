66f32cdbc0c96535dccd063828c4469b1b54dfb2
CrystalxBlade <satish141m@gmail.com>
2026-02-26 21:56:31 +0530

    vector

--- OOPS/5. Dynamic Memory/8_vector/Vector.cpp
+++ OOPS/5. Dynamic Memory/8_vector/Vector.cpp
@@ -7,7 +7,18 @@ int main()
 
     std::vector<int> v1;           // empty
     std::vector<int> v2(5);        //  5 elements
-    std::vector<int> v3(5,0);  
+
+    std::vector<int> v3(5,7);      //  5 elements (7)
+    for(int i : v3)
+    {
+        std::cout << i << std::endl;
+    }
+
+    std::vector<char> v4 {'z', 'u', 's', 'j', 't'};
+    for(char c : v4)
+    {
+        std::cout << c << " ";
+    }
 
     return 0;
 }
