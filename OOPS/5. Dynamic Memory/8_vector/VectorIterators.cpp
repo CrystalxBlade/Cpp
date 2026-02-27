8b294ad31bb16388e6b616ad98163af3069fb530
CrystalxBlade <satish141m@gmail.com>
2026-02-28 00:21:25 +0530

    ve

--- OOPS/5. Dynamic Memory/8_vector/VectorIterators.cpp
+++ OOPS/5. Dynamic Memory/8_vector/VectorIterators.cpp
@@ -3,14 +3,45 @@
 
 int main()
 {
-    std::vector<int> vec = {10, 20, 30, 40};
+    /*
 
+    std::vector<int> vec = {10, 20, 30, 40};
     std::vector<int>::iterator it;
 
     for(it = vec.begin(); it != vec.end(); ++it)
     {
-        std::cout << *it << " ";
+        std::cout << *it << " ";                         //  {10, 20, 30, 40}
+    }
+        
+                // also by using Modern auto
+
+    std::vector<int> vec = {10, 20, 30, 40};
+    for(auto it = vec.begin(); it != vec.end(); ++it)
+    {
+        std::cout << *(it) << " ";
     }
 
+                    // reverse
+
+    std::vector<int> vec = {10, 20, 30, 40};
+    std::vector<int>::reverse_iterator it;
+
+    for(it = vec.rbegin(); it != vec.rend(); ++it)
+    {
+        std::cout << *(it) << " ";                      //  {40, 30, 20, 10}
+    }
+
+             // also by using Modern auto
+
+    std::vector<int> vec = {10, 20, 30, 40};
+    for(auto it = vec.rbegin(); it != vec.rend(); ++it)
+    {
+        std::cout << *(it) << " ";
+    }
+
+    */
+
+    std::vector<int> vec = {10, 20, 30, 40}
+    
     return 0;
 }
