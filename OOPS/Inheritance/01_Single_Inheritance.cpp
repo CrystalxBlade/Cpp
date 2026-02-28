faa8036b8eb676e98e3421f12a37f22d732c9e76
CrystalxBlade <satish141m@gmail.com>
2026-02-28 19:37:08 +0530

    dog

--- OOPS/Inheritance/01_Single_Inheritance.cpp
+++ OOPS/Inheritance/01_Single_Inheritance.cpp
@@ -2,11 +2,32 @@
 
 class Animal
 {
+    public:
+    Animal()
+    {
+        std::cout << "Animal Constructors";
+    }
 
-}
+    void run()
+    {
+        std::cout << " Running ";
+    }
+};
+
+class Dog : public Animal
+{
+    public:
+    void walk()
+    {
+        std::cout << "Dog is walking";
+    }
+};
 
 int main()
 {
+    Dog d;
+    d.run();             // Inherited Function
+    d.walk();           //  Own Function
 
     return 0;
 }
