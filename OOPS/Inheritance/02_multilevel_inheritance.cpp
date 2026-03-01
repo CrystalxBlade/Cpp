4d7fedfcdfe741f788630018160d540df196ec7f
CrystalxBlade <satish141m@gmail.com>
2026-03-01 15:58:08 +0530

    car

--- OOPS/Inheritance/02_multilevel_inheritance.cpp
+++ OOPS/Inheritance/02_multilevel_inheritance.cpp
@@ -9,11 +9,25 @@ class Vehicle
     Vehicle(int s)
     {
         speed = s;
+        std::cout << "Vehicle constructor \n";
+    }
+
+    void move()
+    {
+        std::cout << "Vehicle moving at " << speed << " km/h \n";
     }
 };
 class Car : public Vehicle
 {
-    
+    protected:
+    int wheels;
+
+    public:
+    Car(int s, int w) : Vehicle(s)
+    {
+        wheels = w;
+        std::cout << "Car constructor"
+    }
 };
 
 int main()
