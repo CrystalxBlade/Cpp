a5109f9c50ab7f71fdd7520bc81244cbf1d509c8
CrystalxBlade <satish141m@gmail.com>
2026-03-03 17:16:35 +0530

    smart

--- OOPS/Inheritance/03_multiple.cpp
+++ OOPS/Inheritance/03_multiple.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+class Phone
+{
+    public:
+    void Call()
+    {
+        std::cout << "Calling \n";
+    }
+};
+class Camera
+{
+    public:
+    void Photo()
+    {
+        std::cout << "Phot taken \n";
+    }
+};
+class SmartPhone : public Phone, Camera
+{
+    public:
+    void Internet()
+    {
+        std::cout << "Browsing internet ";
+    }
+};
+
 int main()
 {
+    SmartPhone s;
+    s.Call();
+
     return 0;
 }
