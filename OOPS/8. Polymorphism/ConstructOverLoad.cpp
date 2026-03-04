b1568fcc00ca6f924127dbc7d98705643fbb22c6
CrystalxBlade <satish141m@gmail.com>
2026-03-04 12:25:44 +0530

    crystal

--- OOPS/8. Polymorphism/ConstructOverLoad.cpp
+++ OOPS/8. Polymorphism/ConstructOverLoad.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
 
+class Player
+{
+    private:
+    int health;
+    int level;
+    std::string name;
+
+    public:
+
+     // 1️⃣ No parameters
+    Player()
+    {
+        health = 100;
+        level = 1;
+        name = "Blade";
+        std::cout << "Defalut Player created \n";
+    }
+
+    // 2️⃣ Different NUMBER of parameters
+    Player(int h)
+    {
+        health = h;
+        level = 1;
+        name = "Blade";
+        std::cout << "One parameter constructor \n";
+    }
+
+    // 3️⃣ Different TYPES of parameters
+    Player(int h, int l)
+    {
+        health = h;
+        level = l;
+        name = "Blade";
+        std::cout << "Two parameter constructor \n";
+    }
+
+    // 4️⃣ Same number but DIFFERENT TYPES
+    Player(int h, int l, std::string n)
+    {
+        health = h;
+        level = l;
+        name = n;
+        std::cout << "Three parameter constructor \n";
+    }
+
+    // 5️⃣ Same types but DIFFERENT ORDER
+    Player(std::string n, int l, int h)
+    {
+        health = h;
+        level = l;
+        name = n;
+        std::cout << "Different order parameter constructor \n";
+    }
+
+    void show()
+    {
+        std::cout << "Health: " << health << " - Levle: " << level << " - Name: " << name << '\n'; 
+    }
+};
+
 int main()
 {
+    Player p("Crystal", 2,3);
+    p.show();
+
     return 0;
 }
