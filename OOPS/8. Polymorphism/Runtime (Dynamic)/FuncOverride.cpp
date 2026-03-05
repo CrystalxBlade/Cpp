dec7b2c28469c49113efc573b7e4696d1a44a285
CrystalxBlade <satish141m@gmail.com>
2026-03-05 14:27:38 +0530

    dog

--- OOPS/8. Polymorphism/Runtime (Dynamic)/FuncOverride.cpp
+++ OOPS/8. Polymorphism/Runtime (Dynamic)/FuncOverride.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+class Animal
+{
+    public:
+    void speak()
+    {
+        std::cout << "Animal make sound \n";
+    }
+};
+
+class Dog : public Animal
+{
+    public:
+    void speak()
+    {
+        std::cout << "Dog barks \n";
+    }
+};
+
 int main()
 {
+    
+
     return 0;
 }
