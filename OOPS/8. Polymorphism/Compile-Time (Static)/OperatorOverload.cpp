24b4f074ad8f2d39d65b4a2f8d0dfbf81bdd26cf
CrystalxBlade <satish141m@gmail.com>
2026-03-05 14:15:47 +0530

    n

--- OOPS/8. Polymorphism/Compile-Time (Static)/OperatorOverload.cpp
+++ OOPS/8. Polymorphism/Compile-Time (Static)/OperatorOverload.cpp
@@ -5,12 +5,30 @@ class Vector2
     public:
     int x, y;
 
+    Vector2(int xVal, int yVal) : x(xVal), y(yVal)
+    {
 
+    }
+
+    // Overloading + operator
+    Vector2 operator+(const Vector2& other)
+    {
+        return Vector2(x + other.x, y + other.y);
+    }
+
+    void show()
+    {
+        std::cout << "(" << x << " , " << y << ") \n";
+    }
 };
 
 int main()
 {
-    Vector2 v;
+    Vector2 v1(2, 4);
+    Vector2 v2(6, 8);
+    
+    Vector2 result = v1 + v2;  // Calls overloaded operator
+    result.show();
 
     return 0;
 }
