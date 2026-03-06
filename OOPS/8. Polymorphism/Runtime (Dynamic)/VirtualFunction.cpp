27764b1934a39a94a93cefd76966d5e073cd7800
CrystalxBlade <satish141m@gmail.com>
2026-03-06 15:19:07 +0530

    friend

--- OOPS/8. Polymorphism/Runtime (Dynamic)/VirtualFunction.cpp
+++ OOPS/8. Polymorphism/Runtime (Dynamic)/VirtualFunction.cpp
@@ -24,10 +24,38 @@ int main()
     Animal* ptr;
     Dog d;
 
-    ptr = &d;
-    ptr->speak();
+    ptr = &d;                                       Output
+    ptr->speak();                           //  Animal make a sound
     
     return 0;
 }
 
 */
+
+class Animal
+{
+    public:
+    virtual void speak()
+    {
+        std::cout << "Animal make a sound \n";
+    }
+};
+class Dog : public Animal
+{
+    public:
+    void speak() override
+    {
+        std::cout << "Dog barks \n";
+    }
+};
+
+int main()
+{
+    Animal* ptr;
+    Dog d;
+
+    ptr = &d;                                //   Output
+    ptr->speak();                            // Dog barks
+
+    return 0;
+}
