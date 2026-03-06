2fd8ade16ed79c67c5635078180344a107692bc0
CrystalxBlade <satish141m@gmail.com>
2026-03-06 15:21:07 +0530

    balance

---
+++ OOPS/6. Encapsulation/FriendFunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class BankAccount
+{
+    private:
+    int balance;
+
+    public:
+    BankAccount(int b)
+    {
+        balance = b;
+    }
+
+    
+};
+
+int main()
+{
+    return 0;
+}
