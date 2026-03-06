8b51c7ec906d30098fbd61793638dae061a60396
CrystalxBlade <satish141m@gmail.com>
2026-03-06 15:48:36 +0530

    secret

--- OOPS/6. Encapsulation/FriendClass.cpp
+++ OOPS/6. Encapsulation/FriendClass.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+class Secret
+{
+    private:
+    int code = 6969;
+
+    friend class Hacker;  // Hacker is a friend
+};
+
+class Hacker
+{
+    public:
+    void revealSecret(Secret z)
+    {
+        std::cout << "Secret code: " << z.code; 
+    }
+};
+
 int main()
 {
+    Secret s;
+    Hacker h;
+
+    h.revealSecret(s);
+
     return 0;
 }
