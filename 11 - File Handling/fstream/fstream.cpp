a860596c0dd8b683fa8b7356be76717344672699
CrystalxBlade <satish141m@gmail.com>
2026-03-11 12:18:49 +0530

    file

--- 11 - File Handling/fstream/fstream.cpp
+++ 11 - File Handling/fstream/fstream.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 #include <fstream>
+
+int main()
+{
+    std::fstream file("data.txt", std::ios::out);
+
+    file << "Saving game progress";
+    file.close();
+
+    return 0;
+}
