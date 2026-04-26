99278f725b673179d04e398bf2beb44486a9c522
CrystalxBlade <satish141m@gmail.com>
2026-04-26 12:50:21 +0530

    stringInPair

--- 12 - STL/4. Pair/Pair.cpp
+++ 12 - STL/4. Pair/Pair.cpp
@@ -2,8 +2,14 @@
 
 int main()
 {
-    std::pair<int, std::string> p;
-     
+    /*
 
+    std::pair<int, std::string> p = {4,"Blade"};
+     
+    std::cout << p.first << '\n';
+    std::cout << p.second;
+    */
+
+    
     return 0;
 }
