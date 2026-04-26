b39b1b4f95fbdbd345fb06d4b685f1c5c410039b
CrystalxBlade <satish141m@gmail.com>
2026-04-26 12:55:18 +0530

    pairOfPairs

--- 12 - STL/4. Pair/Pair.cpp
+++ 12 - STL/4. Pair/Pair.cpp
@@ -6,10 +6,16 @@ int main()
 
     std::pair<int, std::string> p = {4,"Blade"};
      
-    std::cout << p.first << '\n';
-    std::cout << p.second;
-    */
-
+    std::cout << p.first << '\n';                 // 4
+    std::cout << p.second;                        // "Blade"
     
+
+    std::pair<int, std::pair<char, int>> p = {5, {'Z',8}};
+
+    std::cout << p.first << '\n';               // 4
+    std::cout << p.second.first << '\n';        // Z
+    std::cout << p.second.second;               // 8
+
+    */
     return 0;
 }
