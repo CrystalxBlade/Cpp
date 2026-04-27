648a4751a712a05f09e4a0f813be03046cef4797
CrystalxBlade <satish141m@gmail.com>
2026-04-27 18:09:06 +0530

    lines

--- 12 - STL/4. Pair/Pair.cpp
+++ 12 - STL/4. Pair/Pair.cpp
@@ -11,20 +11,26 @@ int main()
     std::cout << p.second;                        // "Blade"
     
 
+    -----------------------------------------------------
+
     std::pair<int, std::pair<char, int>> p = {5, {'Z',8}};
 
     std::cout << p.first << '\n';               // 4
     std::cout << p.second.first << '\n';        // Z
     std::cout << p.second.second;               // 8
 
-    */
 
+    ------------------------------------------------------
 
     std::vector<std::pair<int,int>> vec = {{2, 4}, {6,8}, {10, 12}};
 
-    for(auto p : vec)
+    for(std::pair<int,int> p : vec)
     {
         std::cout << p.first << " " << p.second << " " << '\n';
     }
+
+    -------------------------------------------------------
+    */
+   
     return 0;
 }
