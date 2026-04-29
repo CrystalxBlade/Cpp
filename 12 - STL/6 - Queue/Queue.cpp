a120c1de9a50d2ab3c79adcbd0ffb2c38735f809
CrystalxBlade <satish141m@gmail.com>
2026-04-29 17:34:46 +0530

    queue

--- 12 - STL/6 - Queue/Queue.cpp
+++ 12 - STL/6 - Queue/Queue.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <queue>
 
 int main()
 {
+    std::queue<int> q;
+
+    q.push(4);
+    q.push(2);
+    q.push(6);
+
+    while(!q.empty())
+    {
+        std::cout << q.front() << " ";
+        q.pop();
+    }
+    
+
     return 0;
 }
