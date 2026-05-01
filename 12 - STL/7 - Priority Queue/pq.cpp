8d5864b26585eda28cf86e3a0402745bb601cc1a
CrystalxBlade <satish141m@gmail.com>
2026-05-01 21:49:26 +0530

    pop

--- 12 - STL/7 - Priority Queue/pq.cpp
+++ 12 - STL/7 - Priority Queue/pq.cpp
@@ -6,6 +6,15 @@ int main()
     std::priority_queue<int> q;
 
     q.push(2);
+    q.push(5);
+    q.push(7);
+    q.push(9);
+
+    while(!q.empty())
+    {
+        std::cout << q.top() << " ";
+        q.pop();
+    }
 
     return 0;
 }
