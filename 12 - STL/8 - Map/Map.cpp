68a832eb17deefb97c4cdc9b7b89b71a9bf049cc
CrystalxBlade <satish141m@gmail.com>
2026-05-06 19:47:28 +0530

    map

--- 12 - STL/8 - Map/Map.cpp
+++ 12 - STL/8 - Map/Map.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <map>
 
 int main()
 {
+    std::map<std::string, int> m;
+
+    m["tv"] = 100;
+    m["laptop"] = 100;
+    m["headphones"] = 50;
+
+    for(auto p: m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    }
+
     return 0;
 }
