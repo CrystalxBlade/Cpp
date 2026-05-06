a865bca734dd40181dc42e56b2a98544c3ca0c42
CrystalxBlade <satish141m@gmail.com>
2026-05-06 20:19:33 +0530

    erase

--- 12 - STL/8 - Map/Map.cpp
+++ 12 - STL/8 - Map/Map.cpp
@@ -5,6 +5,25 @@ int main()
 {
     std::map<std::string, int> m;
 
+    /*
+                                           print value of a key
+    m["laptop"] = 100;
+    std::cout << m["laptop"];
+
+    ----------------------------------------
+
+                                          count the keys
+    m["tv"] = 100;
+    m["laptop"] = 100;
+    m["headphones"] = 50;
+    m["tablet"] = 120;
+    m["watch"] = 50;
+
+    std::cout << "count: " << m.count("laptop");
+
+    ----------------------------------------
+
+                                          print all keys with values
     m["tv"] = 100;
     m["laptop"] = 100;
     m["headphones"] = 50;
@@ -15,6 +34,45 @@ int main()
     {
         std::cout << p.first << " " << p.second << '\n';
     }
+
+    std::cout << "count: " << m.count("laptop");
+
+    ----------------------------------------
+
+                                          insert & emplace
+    m["tv"] = 100;
+    m["laptop"] = 100;
+    m["headphones"] = 50;
+    m["tablet"] = 120;
+    m["watch"] = 50;
+
+    m.insert({"camera", 50});
+
+    m.emplace("mobile", 100);
+
+    for(auto p: m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    }
+
+    ----------------------------------------
+
+                                        erase
+
+    m["tv"] = 100;
+    m["laptop"] = 100;
+    m["headphones"] = 50;
+    m["tablet"] = 120;
+    m["watch"] = 50;
+
+    m.erase("headphones");
+
+    for(auto p: m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    } 
+        
+    */
 
     return 0;
 }
