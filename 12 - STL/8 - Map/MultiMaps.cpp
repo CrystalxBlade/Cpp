1ec26a4149b0923e56de8b86457f90ef25df7a3e
CrystalxBlade <satish141m@gmail.com>
2026-05-09 21:25:50 +0530

    maps

--- 12 - STL/8 - Map/MultiMaps.cpp
+++ 12 - STL/8 - Map/MultiMaps.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <map>
 
 int main()
 {
+
+    /*
+
+    std::multimap<std::string, int> m;
+
+    m.emplace("tv", 100);
+    m.emplace("tv", 100);
+    m.emplace("tv", 150);
+    m.emplace("tv", 50); 
+
+    m.erase("tv");                      // delete all the keys with same name
+
+    for(auto p : m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    }
+
+    */
+
     return 0;
 }
