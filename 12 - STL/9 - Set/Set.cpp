56122e4aefdb51df893e14dad71dcec5d44e5a36
CrystalxBlade <satish141m@gmail.com>
2026-05-10 20:04:39 +0530

    setOrder

--- 12 - STL/9 - Set/Set.cpp
+++ 12 - STL/9 - Set/Set.cpp
@@ -3,5 +3,17 @@
 
 int main()
 {
+    std::set<int> s;
+
+    s.insert(4);
+    s.insert(2);
+    s.insert(8);
+    s.insert(6);
+
+    for(auto val : s)
+    {
+        std::cout << val << '\n';
+    }
+
     return 0;
 }
