9f9b4885a5d8f57f43a802b370a0ffabf7d816d9
CrystalxBlade <satish141m@gmail.com>
2026-05-10 19:57:31 +0530

    keys

--- 12 - STL/8 - Map/UnOrderedMap.cpp
+++ 12 - STL/8 - Map/UnOrderedMap.cpp
@@ -4,6 +4,32 @@
 int main()
 {
     std::unordered_map<std::string, int> m;
-    
+
+    /*                                            ------- it'll only print one key ❌ 
+
+    m.emplace("tv", 100);
+    m.emplace("tv", 100);
+    m.emplace("tv", 100);
+    m.emplace("tv", 100);
+
+    for(auto p : m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    }
+
+    */
+
+    //                                             -------- different keys ✅ 
+    m.emplace("tv", 120);
+    m.emplace("watch", 100);
+    m.emplace("laptop", 100);
+    m.emplace("ipad", 100);
+
+    for(auto p : m)
+    {
+        std::cout << p.first << " " << p.second << '\n';
+    }
+
+
     return 0;
 }
