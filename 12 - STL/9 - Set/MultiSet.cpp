5a7e5fc5ddb8d02d5862e2ea48b421b2e974db8d
CrystalxBlade <satish141m@gmail.com>
2026-05-11 20:15:56 +0530

    unorderedset

--- 12 - STL/9 - Set/MultiSet.cpp
+++ 12 - STL/9 - Set/MultiSet.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 
 int main()
 {
+    std::unordered_set<int> u;
+
+    u.emplace(2);
+    u.emplace(4);
+    u.emplace(6);
+    u.emplace(8);
+    u.emplace(10);
+
+    for(auto s : u)
+    {
+        std::cout << s << " ";
+    }
+
     return 0;
 }
