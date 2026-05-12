ca4c32df0cd9d1175e2e332502e2bfbbe823897a
CrystalxBlade <satish141m@gmail.com>
2026-05-12 21:13:01 +0530

    insert

--- 12 - STL/9 - Set/MultiSet.cpp
+++ 12 - STL/9 - Set/MultiSet.cpp
@@ -5,5 +5,19 @@ int main()
 {
     std::multiset<int> m;
 
+    m.insert(8);
+    m.insert(2);
+    m.insert(4);
+    m.insert(6);
+
+    m.insert(2);
+    m.insert(4);
+    m.insert(6);
+
+    for(auto i : m)
+    {
+        std::cout << i << " ";
+    }
+
     return 0;
 }
