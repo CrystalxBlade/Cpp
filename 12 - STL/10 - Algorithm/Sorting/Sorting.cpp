4a440a5942fc73d29daa02bb012b86feb0159750
CrystalxBlade <satish141m@gmail.com>
2026-05-13 19:57:50 +0530

    vector

--- 12 - STL/10 - Algorithm/Sorting/Sorting.cpp
+++ 12 - STL/10 - Algorithm/Sorting/Sorting.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main()
 {
+    /*
+
+    int arr[5] = {3, 5, 1, 8, 2};
+
+    std::sort(arr, arr + 5);
+
+    for(int val : arr)
+    {
+        std::cout << val << " ";
+    }
+
+    */
+
+    std::vector<int> vec = {3, 5, 1, 8, 2};
+
+    std::sort(vec.begin(), vec.end());
+
+    for(int val: vec)
+    {
+        std::cout << val << " ";
+    }
+
     return 0;
 }
