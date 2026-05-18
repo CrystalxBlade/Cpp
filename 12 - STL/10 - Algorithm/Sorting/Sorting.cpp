9eabcbf97ac64a1d1958f33a35f80fbd6a1a33bb
CrystalxBlade <satish141m@gmail.com>
2026-05-18 20:27:57 +0530

    first

--- 12 - STL/10 - Algorithm/Sorting/Sorting.cpp
+++ 12 - STL/10 - Algorithm/Sorting/Sorting.cpp
@@ -59,7 +59,9 @@ int main()
      bool comparator(std::pair<int, int> p1, std::pair<int, int> p2)
     {
         if(p1.second < p2.second) return true;
-        if(p1.second > p2.second) return true;
+        if(p1.second > p2.second) return false;
+
+        if(p1.first < p2.first) return true;
         else return false;
     }
 
